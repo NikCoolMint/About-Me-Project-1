72ae5ca6ae8929e95f2f6909eb670bf68e71f60a
NikCoolMint <demnuts911@gmail.com>
2025-11-11 16:02:35 -0500

    Fix conditional for About page in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,8 @@ with col2:
     st.image('ZombieManSpace.jpg', use_column_width=True)
 
 # About Page
-  elif page == '🤠 About':
+elif:
+  page == '🤠 About':
   st.title('About Me')
 
   # Timeline of my Professional Journey
