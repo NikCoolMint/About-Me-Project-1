17701c8798632a5d969af103512b9d7ea6f22069
NikCoolMint <demnuts911@gmail.com>
2025-11-12 11:37:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ if page == '🚶🏾‍♂️ About':
     st.title('About Me')
 
   # Timeline of my Professional Journey
-  st.subheader('My Journey 🗺️')
+    st.subheader('My Journey 🗺️')
 
   with st.expander('2025 - Present: Medgar Evers College'):
     st.write('''
