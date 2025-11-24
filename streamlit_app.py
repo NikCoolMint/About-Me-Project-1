bc7a8dd2ed621dfc917aa2a6635a2c9729590d41
NikCoolMint <demnuts911@gmail.com>
2025-11-24 17:10:44 +0000

    coommit

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ st.markdown(page_bg, unsafe_allow_html=True)
 sidebar_bg = """
 <style>
 [data-testid="stSidebar"] {
-    background-color: #222222; /* 999966 */
+    background-color: #222222; /* #999966 */
 }
 </style>
 """
